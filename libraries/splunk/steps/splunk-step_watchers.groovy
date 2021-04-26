b43d890cf737d7fa1ea561568badff0d84fa7939
krrishkishore <krrishkishore@gmail.com>
2021-04-26 16:24:03 +0530

    Removing the unwanted file

--- libraries/splunk/steps/splunk-step_watchers.groovy
+++
@@ -1,9 +0,0 @@
-@BeforeStep
-void before(){
-    println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
-}
-
-@AfterStep
-void after(){
-    println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
-}
