c52a824b3928fe55d3f8017003f84e49efb868b9
Kishore <kishore_ari@yahoo.com>
2022-05-30 01:25:28 +0530

    Update build.groovy

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,4 +1,6 @@
+@Init
 void call(){
+    additionalParams << booleanParam(name: 'PUSH_RELEASE_ARTIFACTS', defaultValue: false, description: 'Deploy to release repository')
     stage("Maven: Build"){
         println "build from the maven library"
     }
